13eec3956f6cb4a2713202439261531b1b470087
duncan <costes.duncan@gmail.com>
2024-08-21 16:54:15 +0200

    ajout route choice et scenario dans server

--- back/server.js
+++ back/server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const unicornRoute = require('./routes/unicornRoute');
 const userRoute = require('./routes/userRoute')
 const scenarioRoute = require('./routes/scenarioRoute')
+const storyRoute = require('./routes/storyRoute')
+const scenarioRoute = require('./routes/scenarioRoute')
+const choiceRoute = require('./routes/choiceRoute')
 
 app.use(express.json());
 app.use(cors());
@@ -11,6 +14,8 @@ app.use(cors());
 app.use('/api/unicorn/', unicornRoute);
 app.use('/api/user/', userRoute);
 app.use('/api/scenario/', scenarioRoute);
+app.use('/api/story/', storyRoute)
+app.use('/api/choice/', choiceRoute)
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
