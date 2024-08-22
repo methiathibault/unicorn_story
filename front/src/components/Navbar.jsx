eba8a5d7506e53d5e3d533dda42c652b70937018
duncan <costes.duncan@gmail.com>
2024-08-22 12:10:53 +0200

    story page debut

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -4,6 +4,8 @@ export default function Navbar() {
     return (
         <div>
             <Link to="/">Home</Link>
+            <Link to="/story"> story</Link>
+            <Link to="/login"> login</Link>
         </div>
     )
 }
--- front/src/pages/StoryPage.jsx
+++ front/src/pages/StoryPage.jsx
@@ -1,4 +1,4 @@
-import React,{useState, useContext} from 'react'
+import React,{useState, useEffect} from 'react'
 import axios from 'axios'
 import ScenarComponent from '../components/ScenarComponent'
 import ChoiceComponent from '../components/ChoiceComponent'
@@ -6,17 +6,70 @@ import ChoiceComponent from '../components/ChoiceComponent'
 
 export default function StoryPage() {
    
-    const[storyId,setStoryId] = useState[1]
+    const[storyId,setStoryId] = useState(1)
+    const [scenario,setScenario] = useState([])
+    const [currentScenario,setCurrentScenario] = useState([])
+    const [turn, setTurn] = useState(1)
+    let test = []
 
-    function getScenar(){
-        axios.get("http://localhost:3000/")
+    
+
+    async function getScenar(){
+        axios.get("http://localhost:8000/api/scenario/scenario/story/"+storyId)
+        .then(res => setScenario(res.data))
+        .catch(err => console.log(err))
+        test = await  axios.get("http://localhost:8000/api/scenario/scenario/story/"+storyId)
+       // console.log(test.data)
+        await startScenar(test.data)
     }
+    
+    
+  
+
+    function nextScenar(id){
+
+    }
+
+    async function startScenar(scene){  
+      // console.log(scene)
+      // console.log("start scenar "+scene+ "end ")
+      scene.map((sce)=>{
+        //console.log(sce)
+        if(sce.difficulty === 1){
+          setCurrentScenario(sce)
+        }
+      })
+
+    }
+
+    useEffect(()=>{
+      getScenar()
+     // startScenar(1)
+    },[])
 
   return (
     <div>
         <h1>this is a story 1</h1>  
-        <ScenarComponent/>
+        <ScenarComponent />
 
+        {console.log("ble")}
+        {console.log(currentScenario)}
+
+        {currentScenario.title}
+        {currentScenario.description}
+        {currentScenario.difficulty}
+
+        {/* {currentScenario.map((sce)=>(
+
+          <>
+
+            <div>{sce.title}</div>
+          </>
+        ))} */}
+
+        
+
+       
        
     </div>
   )
