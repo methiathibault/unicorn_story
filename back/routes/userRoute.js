74301f9b6eaf9f16127215e28b18db8fb76e28cc
duncan <costes.duncan@gmail.com>
2024-08-22 15:50:08 +0200

    modification du middleware

--- back/routes/userRoute.js
+++ back/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const middleware = require("../controllers/middleware")
 
 const userController = require('../controllers/userController')
 
-router.get('/config', userController.createTableUnicorn);
+router.get('/config', middleware.authenticate, userController.createTableUnicorn);
 router.post("/login",userController.login)
 router.post("/register",userController.register)
 
